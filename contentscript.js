61983e405b0b4b6588b6579dc87561dce2f0cd84
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-08-06 23:44:10 -0400

    refactor code

--- contentscript.js
+++ contentscript.js
@@ -4,28 +4,17 @@
 var webpageBody = $(document).find("body");
 const webpageBodyText = webpageBody.text();
 
-var webpageBodyText = webpageBody.text();
-
+// Removes any extra white spaces to significantly reduce length count
 const webpageTextTrim = webpageBodyText.replace(/\s+/g, ' ').trim();
 
-function chunkString(str, length) {
+// Breaks text into chunk sizes the API can handle 
+const chunkString = (str, length) => {
   return str.match(new RegExp('.{1,' + length + '}', 'g'));
 }
-
 const strChunksArr = chunkString(webpageTextTrim, 5120)
-console.log(strChunksArr);
 
-var apiKey = "";
-
-var params = {
-  showStats: "true",
-};
-
-
-let documentId = 0;
 const documentsArr = []
-const requestContentResult = strChunksArr.map((chunk) => {
-  documentId ++;
+strChunksArr.map((chunk, index) => {
   documentsArr.push(
     {
       language: "en",
@@ -33,17 +22,24 @@ const requestContentResult = strChunksArr.map((chunk) => {
       text: chunk, // Text being sent to analyze.
     }
   )
-})
-console.log(documentsArr)
+});
 
-var requestContent = {
+// Request data
+const requestContent = {
   "documents": documentsArr,
-}
+};
 
 // Header containing API Key
+const apiKey = "";
+
+// URL parameter query 
+const params = {
+  showStats: "true",
+};
 
 // ====== SEND REQUEST TO SERVER ====== //
 
+// The following code is taken from: https://westcentralus.dev.cognitive.microsoft.com/docs/services/TextAnalytics-v2-1/operations/56f30ceeeda5650db055a3c9
 $.ajax({
   url:
     "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.1/sentiment?" + $.param(params),
@@ -53,51 +49,37 @@ $.ajax({
   },
   type: "POST",
   data: JSON.stringify(requestContent),
-  data: JSON.stringify(requestContent1),
 })
   .done(( {documents} ) => {
     // ====== DETERMINE RATING ====== //
-  .done(function (data) {
-
-    var returnedDocuments = data.documents;
 
     // Let's average the sentiment scores so we can have a singular 'positive' 'neutral' 'negative' flags we can show
     // to the users
-    var totalScore = 0;
-    var numberOfScores = 0;
-
-    returnedDocuments.forEach((item) => {
+    let totalScore = 0;
+    let analyticsResult = "";
+    documents.forEach((item) => {
       totalScore = totalScore + item.score;
-      numberOfScores++;
     });
 
-    var averageScore = totalScore / numberOfScores;
-    console.log("AVG:", averageScore)
     // Ascording to Azure Sentiment Analysis, score of 0.5 is absulte neutral, 0 is more negative, 1 is most positive.
     const averageScore = totalScore / documents.length;
     if (averageScore >= 0.4 && averageScore <= 0.6) {
       analyticsResult = "NEUTRAL";
-      analysisResultElement.innerHTML = "NEUTRAL";
+    } else if (averageScore < 0.4) {
       analyticsResult = "NEGATIVE";
-      analysisResultElement.style.color = "white";
-    }
-
+    } else if (averageScore > 0.6) {
+      analyticsResult = "POSITIVE";
+    } else {
       analyticsResult = "Undetermined";
-    if (averageScore < 0.4) {
-      analysisResultElement.innerHTML = "NEGATIVE";
-      analysisResultElement.style.color = "red";
     }
-
-    if (averageScore > 0.6) {
-      analysisResultElement.innerHTML = "POSITIVE";
-      analysisResultElement.style.color = "green";
-    }
-    chrome.storage.sync.set({ analysisResultElement: analysisResultElement.innerHTML }, function () {
-      console.log('The element is ready.');
-    })
+    chrome.storage.sync.set({ analyticsResult }, () => {
+      return analyticsResult;
+    });
 
   })
   // If the server request fails, we will need to handle the case here in .fail method.
-  .fail(function () {
-    alert("error");
+  .fail(() => {
+    if (documentsArr.length = 0) alert("Error - unable to scrap any text. Try another site.");
+    else if (apiKey === "") alert("Error - the API isn't working, are you using a valid Azure API key?");
+    else alert("error");
   });
